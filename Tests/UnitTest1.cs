9723cb5aec497b552d74159a6f6c3812d6c2ec33
jhall0310 <jonathanphall@me.com>
2017-12-14 14:25:54 -0800

    first commit

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+
+        [TestMethod]
+        public void Can_Go_To_HomePage()
+        {
+            Pages.Homepage.Goto();
+            Assert.IsTrue(Pages.Homepage.IsAt());
+        }
+    }
+
+    internal class Pages
+    {
+        public class Homepage
+        {
+            public static void Goto()
+            {
+                throw new NotImplementedException();
+            }
+
+            public static bool IsAt()
+            {
+                throw new NotImplementedException();
+            }
+
+        }
+
+
+    }
+}
